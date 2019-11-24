9a84e9b433a96ff2c34bd6d62924c4f8fce19c12
run <run@nous.com>
2019-11-24 17:51:22 +0100

    fix dir scan

--- index.php
+++ index.php
@@ -1,39 +1,72 @@
 <html>
     <head>
-
+	<style>
+		div.title
+		{
+			text-align: center;
+			margin-left: auto;
+			margin-right: auto;
+			margin-bottom: 50px;
+		}
+		div.head
+		{
+			width: 300px;
+			background-color: lightgrey;
+			border-style: solid;
+			border-color: lightgrey;
+			border-width: 1px;
+			text-align: center;
+			padding: 5px;
+			margin-left: auto;
+			margin-right: auto;
+		}
+		div.content
+		{
+			width: 300px;
+			border-color: lightgrey;
+			border-style: solid;
+			border-width: 1px;
+			margin-bottom: 50px;
+			padding: 5px;
+			margin-right: auto;
+			margin-left: auto;
+			margin-bottom: 50px;
+		}
+	</style>
     </head>
     <body>
         <div class="title">Mr Grojean</div>
         <?php
             function PrintDir($path, $dir)
-            {
+	    {
                 $fullPath = $path . "/" . $dir;
-                $files = scandir($fullPath);
+		$files = scandir($fullPath);
 
                 foreach ($files as $value)
                 {
-                    if(!is_dir($value))
+                    if(!is_dir($fullPath . "/" . $value))
                     {
                         $rootFiles[] = $value;
-                    }
-                    PrintDir($fullPath . "/" . $value);
+		    }
+		    else if($value != "." && $value != "..")
+		    {
+                    	PrintDir($fullPath, $value);
+		    }
                 }
 
                 if(count($rootFiles) > 0)
                 {
-                    echo("<div>" . $dir ."</div>");
-                    echo("<div>");
+                    echo("<div class='head'>" . $dir ."</div>");
+                    echo("<div class='content'>");
                     foreach ($rootFiles as $value)
                     {
-                        echo("<a href='Fichiers/" . $value . "'>" . $value . "</a>");
+                        echo("<a href='" . $fullPath . "/" . $value . "'>" . $value . "</a>");
                     }
                     echo("</div>");
                 }
             }
 
-            PrintDir("./Fichiers");
-
-            $files = scandir("./Fichiers");
+            PrintDir("./Fichiers", ".");
         ?>
     </body>
 </html>
